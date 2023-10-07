8b41ca9d18e7409c904f7ace2256a7a41790bd5e
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2023-10-08 03:21:39 +0530

    day 13 is completed

--- day12Ex.js
+++ day12Ex.js
@@ -1,22 +1,22 @@
 /* Level 1 */
 
-/* 
+
 // 1) Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
 const incomeText = "He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.";
 const incomeArray = incomeText.match(/\d+/g).map(Number);
 const totalIncome = incomeArray.reduce((acc,val)=>acc+val,0);
 console.log(`Total Incomne Is : ${totalIncome} Euro`);
- */
 
-/* 
+
+
 // 2) The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
 const points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8'];
 const sortedPoint = points.map(Number).sort((a,b)=>a-b);
 const distance = sortedPoint[sortedPoint.length-1]-sortedPoint[0];
 console.log(distance);
- */
 
-/* 
+
+
 // 3) Write a pattern which identify if a string is a valid JavaScript variable
 
 function is_valid_variable(str){
@@ -28,11 +28,11 @@ console.log(is_valid_variable('firstName'));
 console.log(is_valid_variable('FirstName'));
 console.log(is_valid_variable('0firstName'));
 console.log(is_valid_variable('_firstName$'));
- */
+
 
 
 /* Level 2 */
-/* 
+
 // 1) Write a function called tenMostFrequentWords which get the ten most frequent word from a string?
 
         function tenMostFrequentWords(paragraph){
@@ -46,7 +46,6 @@ console.log(is_valid_variable('_firstName$'));
                 wordFrequency[word]=1;
             }
         });
-        console.log(wordFrequency);
 
         //convert to wordFrequency object into array
         const wordFrequencyArray = Object.keys(wordFrequency).map((word)=>({
@@ -65,4 +64,3 @@ console.log(is_valid_variable('_firstName$'));
     paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
 
     console.log(tenMostFrequentWords(paragraph));
- */
---
+++ exercise.js
@@ -0,0 +1,28 @@
+/* 
+// Sample data
+const countries = [
+    { name: 'USA', population: 331002651, area: 9525067 },
+    { name: 'China', population: 1439323776, area: 9596961 },
+    { name: 'India', population: 1380004385, area: 3287263 },
+    // Add more countries as needed
+  ];
+  
+  const countriesObject = {
+    USA: { population: 331002651, area: 9525067 },
+    China: { population: 1439323776, area: 9596961 },
+    India: { population: 1380004385, area: 3287263 },
+    // Add more countries as needed
+  };
+  
+  // Group logs using console.group()
+  console.group('Countries Data');
+  console.group('Array Data');
+  console.table(countries); // Display the array as a table
+  console.groupEnd(); // End of Array Data group
+  
+  console.group('Object Data');
+  console.table(countriesObject); // Display the object as a table
+  console.groupEnd(); // End of Object Data group
+  
+  console.groupEnd(); // End of Countries Data group
+   */
