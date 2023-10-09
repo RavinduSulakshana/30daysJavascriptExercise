c9371880f685e82e7277ee3dd8a6e871d42cdf04
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2023-10-10 01:07:32 +0530

    day 15

--- day15Ex.js
+++ day15Ex.js
@@ -1,13 +1,55 @@
-class Person {
+/* Level 1 */
 
-    constructor(firstName,lastName){
-        console.log(this);
-        this.firstName=firstName;
-        this.lastName= lastName;
+
+// 1) Create an Animal class. The class will have name, age, color, legs properties and create different methods
+class Animal{
+    constructor(name,age,color,legs){
+        this.name=name;
+        this.age=age;
+        this.color=color;
+        this.legs=legs;
+    }
+
+    set setName(name){
+        this.name=name;
+    }
+
+    set setAge(age){
+        this.age=age;
+    }
+    set setColor(color){
+        this.color=color;
+    }
+    
+    set setLegs(legs){
+        this.legs=legs;
+    }
+
+    get getName(){
+        return this.name;
+    }
+
+    get getAge(){
+        return this.age;
+    }
+    get getColor(){
+        return this.colorlor;
+    }
+    
+    get getLegs(){
+        return this.legs;
     }
 }
 
-const person = new Person('ravindu','sulakshana');
-const person2 =new Person("Thiwanya","dewmini");
-console.log(person)
-console.log(person2)
+// 2) Create a Dog and Cat child class from the Animal Class.
+class Dog extends Animal{
+
+}
+class Cat extends Animal{
+
+}
+
+/* Level 2 */
+
+// 1) Override the method you create in Animal class
+
