679b52a12870c653131ca7c35e0c5a929275a217
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2024-08-28 09:58:38 +0530

    day9 exercise is started

---
+++ day9/exercise3.js
@@ -0,0 +1,7 @@
+const { countries_data } = require("../countries_data");
+
+//Use the countries information, in the data folder. Sort countries by name, by capital, by population
+
+console.log(countries_data);
+
+//*** Find the 10 most spoken languages:
